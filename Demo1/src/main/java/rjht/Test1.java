77d88220b50c3210719185f4b99223c4a2179b04
liaoxuan <1453336895@qq.com>
2020-11-08 16:31:56 +0800

    学习逻辑运算

---
+++ Demo1/src/main/java/rjht/Test1.java
@@ -0,0 +1,22 @@
+package rjht;
+
+/**
+ * @Description 细节决定成败
+ * @Date 2020/11/8 15:29
+ * @Param 仁聚汇通信息科技有限责任公司
+ * @Author liaoxuan
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        /*
+        强制类型转换
+         */
+        short i = 10;
+        int s ;
+        s = i + 3;
+        i = (short)(i + 3);
+        System.out.println("s"+"="+s);
+        System.out.println("i"+"="+i);
+
+    }
+}
